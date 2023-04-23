f7dc6f6889278d9a113eb63c19271acbe36635a8
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-23 12:16:42 -0700

    Formatted

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var visit = require('unist-util-visit');
+var visit = require("unist-util-visit")
 
-module.exports = attacher;
-attacher.withInlineCodeLanguage = withInlineCodeLanguage;
+module.exports = attacher
+attacher.withInlineCodeLanguage = withInlineCodeLanguage
 
 function attacher(separator) {
-  if (!separator) return;
+	if (!separator) return
 
-  return transformer;
+	return transformer
 
-  function transformer(tree) {
-    visit(tree, 'inlineCode', function visitor(node) {
-      withInlineCodeLanguage(node, separator);
-    });
-  }
+	function transformer(tree) {
+		visit(tree, "inlineCode", function visitor(node) {
+			withInlineCodeLanguage(node, separator)
+		})
+	}
 }
 
 function withInlineCodeLanguage(node, separator) {
-  const [language, code] = node.value.split(separator, 2);
-  if (language && code) {
-    node.value = code;
-    node.lang = language.trim();
-  }
-  return node;
+	const [language, code] = node.value.split(separator, 2)
+	if (language && code) {
+		node.value = code
+		node.lang = language.trim()
+	}
+	return node
 }
