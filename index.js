5e0e3ec1471204cb1283fb108a682392929c3d2e
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-23 18:58:01 -0700

    Added advanced options - WIP

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 import { visit } from "unist-util-visit"
 
-export default function attacher(separator) {
-	if (!separator) return
+const SEPARATOR_POSITION = {
+	BEFORE: "before",
+	AFTER: "after",
+	BOTH: "both",
+}
 
+/**
+ * @typedef Options
+ * @prop {...SEPARATOR_POSITION} separator_position
+ * @prop {string} separator_character
+ */
+
+/** @type {Options} */
+const default_options = {
+	separator_position: SEPARATOR_POSITION.BEFORE,
+	separator_character: "_",
+}
+
+/**
+ * @param {Options} options
+ */
+export default function attacher(options = default_options) {
 	return function transformer(tree) {
 		visit(tree, "inlineCode", function visitor(node) {
-			within_inline_code_language(node, separator)
+			within_inline_code_language(node, options)
 		})
 	}
 }
 
-function within_inline_code_language(node, separator) {
-	const [language, code] = node.value.split(separator, 2)
-	if (language && code) {
-		node.value = code
-		node.lang = language.trim()
+function within_inline_code_language(node, options) {
+	if (options.separator_position === SEPARATOR_POSITION.BEFORE) {
+		const match = node.value.match(
+			get_separator_position_before_regex(options.separator_character)
+		)
+
+		if (match) {
+			const language = match[1]
+			const code = match[2]
+
+			node.value = code
+			node.lang = language
+		}
 	}
+
 	return node
+}
+
+function get_separator_position_before_regex(
+	separator_character = default_options.separator_character
+) {
+	// Example: /^_[a-z]+\s+.+$/i
+	const new_regex_string = `^${separator_character}([a-z]+)\\s+(.+)$`
+	const new_regex = new RegExp(new_regex_string, "i")
+	return new_regex
 }
